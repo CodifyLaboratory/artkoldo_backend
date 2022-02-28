87dfffeafb3756a456b1f2373da23f74a8e4e1e9
amanaau19 <abdyraev.aman@gmail.com>
2022-02-28 05:36:49 +0000

    static and media settings

--- artkoldo_backend/settings.py
+++ artkoldo_backend/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/back-static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'back-static/')
 
 # Default primary key field type
