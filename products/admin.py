417fad256f7635568fe3bdc7488f775e3aedfb16
TIP-ROK <chenye797@gmail.com>
2022-03-12 17:15:37 +0600

    modified product models, validation for uniqueness, ordering by date

--- products/admin.py
+++ products/admin.py
@@ -19,6 +19,10 @@ class CountryAdmin(admin.ModelAdmin):
     list_filter = ('country',)
 
 
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', 'created_at',)
+
+
 admin.site.register(Country, SiteAdmin)
 admin.site.register(Region, CountryAdmin)
 admin.site.register(Author, AuthorAdmin)
@@ -28,14 +32,14 @@ admin.site.register(Subject, SiteAdmin)
 admin.site.register(PaintMaterial, SiteAdmin)
 admin.site.register(Style, SiteAdmin)
 admin.site.register(PaintTechnique, SiteAdmin)
-admin.site.register(Painting, SiteAdmin)
+admin.site.register(Painting, ProductAdmin)
 
 admin.site.register(HandicraftType, SiteAdmin)
 admin.site.register(HandicraftMaterial, SiteAdmin)
 admin.site.register(HandicraftTechnique, SiteAdmin)
-admin.site.register(Handicraft, SiteAdmin)
+admin.site.register(Handicraft, ProductAdmin)
 
 admin.site.register(CeramicType, SiteAdmin)
 admin.site.register(CeramicMaterial, SiteAdmin)
 admin.site.register(CeramicTechnique, SiteAdmin)
-admin.site.register(Ceramic, SiteAdmin)
+admin.site.register(Ceramic, ProductAdmin)
