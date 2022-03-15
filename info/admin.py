821154149c91572418c88833d1222a6deca1d6dd
TIP-ROK <chenye797@gmail.com>
2022-03-15 21:33:51 +0600

    added new fields(photo_1,2,3,4,5) to product models

--- info/admin.py
+++ info/admin.py
@@ -9,6 +9,7 @@ class InfoAdmin(admin.ModelAdmin):
         else:
             return True
 
+
 class FounderAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         if self.model.objects.count() == 2:
