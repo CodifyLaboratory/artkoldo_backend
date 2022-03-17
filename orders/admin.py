0002bccbcdd1e46d3535acb1b4a2b71c33e05036
Aman Abdyraev <abdyraev.aman@gmail.com>
2022-03-17 16:21:22 +0600

    United order (#4)
    * customized admin and cleaned code
    
    * little tweak in products models for proper function of orders
    
    * now product serializers send category name
    
    * modified order

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
-from django.urls import reverse
 from django.utils.safestring import mark_safe
-from products.models import Painting, Handicraft, Ceramic
-from .models import Order, OrderStatus
+from .models import Order, OrderStatus, OrderProduct
+
+
+class OrderStatusAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    extra = 0
+    verbose_name_plural = "Товары"
+    readonly_fields = ('product_link', 'quantity', 'price')
+    fields = ('product_link', 'quantity', 'price')
+
+    def product_link(self, obj):
+        return mark_safe(obj.link)
+    product_link.short_description = 'Товар'
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 class OrderStatusAdmin(admin.ModelAdmin):
@@ -10,45 +30,23 @@ class OrderStatusAdmin(admin.ModelAdmin):
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('name', 'order_status', 'products_category', 'country', 'created_date')
+    inlines = [OrderProductInline, ]
+    list_display = ('name', 'id', 'order_status', 'country', 'created_date')
     list_filter = ('order_status', 'name', 'phone')
 
     fieldsets = (
         ('Покупатель', {
             'fields': ('name', 'email', 'phone', 'country', 'region', 'city')
         }),
-        ('Товар', {
-            'fields': ('get_product', 'quantity', 'price', 'total_price'),
-        }),
-        (None, {
-            'fields': ('comment', 'created_date', 'order_status'),
+        ('О заказе', {
+            'fields': ('comment', 'created_date', 'order_status', 'total_price'),
         })
     )
 
-    def get_product(self, obj):
-        prod_name = ''
-        if obj.products_category == 'painting':
-            prod_name = f"Картина: {Painting.objects.get(id=obj.products_id)}"
-        elif obj.products_category == 'handicraft':
-            prod_name = f"Ремесленное изделие: {Handicraft.objects.get(id=obj.products_id)}"
-        elif obj.products_category == 'ceramic':
-            prod_name = f"Керамика: {Ceramic.objects.get(id=obj.products_id)}"
-
-        display_text = ", ".join([
-            "<a href={}>{}</a>".format(
-                reverse('admin:{}_{}_change'.format('products', obj.products_category),
-                        args=(obj.products_id,)),
-                prod_name)
-        ])
-        if display_text:
-            return mark_safe(display_text)
-        return "-"
-    get_product.short_description = 'Товар'
-
     def get_readonly_fields(self, request, obj=None):
         if obj:
-            return ['name', 'email', 'phone', 'country', 'region', 'city', 'comment', 'created_date', 'total_price',
-                    'quantity', 'price', 'products_id', 'product_name', 'get_product']
+            return ['name', 'email', 'phone', 'country', 'region', 'city', 'comment', 'created_date', 'get_product',
+                    'total_price']
         else:
             return []
 
