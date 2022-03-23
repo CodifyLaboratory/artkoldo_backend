ce2e94dd8c389f9f180844b97f593da4153f5ec1
amanaau19 <abdyraev.aman@gmail.com>
2022-03-23 13:34:23 +0600

    added email sending after order creation

---
+++ orders/signals.py
@@ -0,0 +1,26 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Order
+from django.conf import settings
+from django.urls import reverse
+from django.core.mail import send_mail
+
+
+@receiver(post_save, sender=Order)
+def order_created(instance, created, **kwargs):
+    if created:
+        link = ", ".join([
+            "http://127.0.0.1:8000{}".format(
+                reverse('admin:{}_{}_change'.format('orders', 'order'),
+                        args=(instance.id,)),
+            )
+        ])
+        message = f"Номер заказа: {instance.id} \n" \
+                  f"Имя: {instance.name} \n" \
+                  f"Ссылка на заказ в админ панели: {link}"
+        send_mail(
+            subject=f"Заказ №{instance.id}",
+            message=message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=["aman.aau19@gmail.com"]
+        )
