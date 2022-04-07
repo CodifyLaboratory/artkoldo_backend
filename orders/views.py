5cd7104f9948a5b86786c9c3b8bfbfd92ba13096
amanaau19 <abdyraev.aman@gmail.com>
2022-04-07 17:36:10 +0600

    deleted csrf_exempt

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,8 @@
 from rest_framework import viewsets
 from .serializers import OrderSerializer
 from .models import Order
-from django.views.decorators.csrf import csrf_exempt
 
 
-@csrf_exempt
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
